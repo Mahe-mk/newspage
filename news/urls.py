82b6fd377a91ac406bcab3fb6767a65d723b09c9
Mahe-mk <mahemk07@gmail.com>
2023-02-24 18:38:38 +0530

    Finished the View asn Class_based

--- news/urls.py
+++ news/urls.py
@@ -4,20 +4,20 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from news import views
+from.views import Home,All_news,Categories,AllCategory,Detail,Category,SignUpView,WeatherView
 
 
 urlpatterns=[
-    path('',views.home,name='home'),
-    path('all-news',views.all_news,name='all-news'),
-    path('detail/<int:id>',views.detail,name='detail'),
-    path('all-category',views.all_category,name='all-category'),
-    path('category/<int:id>',views.category,name='category'),
+    path('',Home.as_view(),name='home'),
+    path('all-news',All_news.as_view(),name='all-news'),
+    path('detail/<int:id>',Detail.as_view(),name='detail'),
+    path('all-category',AllCategory.as_view(),name='all-category'),
+    path('category/<int:id>',Category.as_view(),name='category'),
     path('signin', auth_views.LoginView.as_view(template_name='signin.html'), name='signin'),
     path('signout', auth_views.LogoutView.as_view(template_name='signout.html'), name='signout'),
-    path('signup',views.signup,name='signup'),
-    path('weather',views.weather,name='weather'),
+    path('signup',SignUpView.as_view(),name='signup'),
+    path('weather',WeatherView.as_view(),name='weather'),
     path("", include("allauth.urls")),
-
 
 ] 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib import messages
 from .models import News, Categories
 from rest_framework.views import APIView
@@ -8,91 +8,115 @@ from .forms import UserRegistrationForm
 from django.http import HttpResponse
 from django.contrib.auth import logout
 from django.contrib.auth.forms import AuthenticationForm
+from django.views import View
 import requests
 # Create your views here.
 
-def home(request):
-    first_news=News.objects.first()
-    three_news=News.objects.all()[1:3]
-    three_categories=Categories.objects.all()[0:3]
-    return render(request,'home.html',{
-        'first_news':first_news,
-        'three_news':three_news,
-        'three_categories':three_categories
-    })
+class Home(View):
+     def get(self, request, *args,**kwargs):
+        first_news = News.objects.first()
+        three_news = News.objects.all()[1:3]
+        three_categories = Categories.objects.all()[0:3]
+        context = {
+            'first_news': first_news,
+            'three_news': three_news,
+            'three_categories': three_categories
+        }
+        return render(request, 'home.html', context)
 
-# All News
-def all_news(request):
-    all_news=News.objects.all()
-    return render(request,'all-news.html',{
-        'all_news':all_news
-    })
 
+class All_news(View):
+    def get(self, request):
+        all_news=News.objects.all()
+        return render(request, 'all-news.html', {
+            'all_news': all_news
+        })
+     
 # fetch the detailed full news 
-def detail(request,id):
-    news=News.objects.get(id=id)
-    category=Categories.objects.get(id=news.category.id)
-    return render(request,'detail.html',{
-        'news':news,
-    })
 
+class Detail(View):
+    def get(self, request, id):
+        news = get_object_or_404(News, id=id)
+        category = Categories.objects.get(id=news.category.id)
+        return render(request, 'detail.html', {
+            'news': news,
+        })
+        
 
 # Fetch all category 
-def all_category(request):
-    cats=Categories.objects.all()
-    return render(request,'category.html',{
-        'cats':cats
-    })
+
+class AllCategory(View):
+    def get(self, request):
+        cats = Categories.objects.all()
+        return render(request, 'category.html', {
+            'cats': cats
+        })
+    
 
 # Fetch all news from selected category
-def category(request,id):
-    category=Categories.objects.get(id=id)
-    news=News.objects.filter(category=category)
-    return render(request,'category-news.html',{
-        'all_news':news,
-        'categories':category
-    })
 
+class Category(View):
+    def get(self, request, id):
+        category = get_object_or_404(Categories, id=id)
+        news = News.objects.filter(category=category)
+        return render(request, 'category-news.html', {
+            'all_news': news,
+            'categories': category
+        })    
+    
 # To Signup the user for login
 
-def signup(request):
-    if request.method == 'POST':
+class SignUpView(View):
+    def get(self, request):
+        form = UserRegistrationForm()
+        context = {'form': form}
+        return render(request, 'signup.html', context)
+    
+    def post(self, request):
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
-            form.save()  
+            form.save()
             return redirect('signin')
-    else:
-        form = UserRegistrationForm()
-    context = {'form': form}
-    return render(request, 'signup.html', context)
-
-
+        else:
+            context = {'form': form}
+            return render(request, 'signup.html', context)
+        
 # To Signin the user
-def signin(request):
-	if request.method == "POST":
-		form = AuthenticationForm(request, data=request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password')
-			user = authenticate(username=username, password=password)            
-		else:
-			messages.error(request,"Invalid username or password.")
-	form = AuthenticationForm()
-	return render(request=request, template_name="signin.html", context={"signin_form":form},)
 
+class SignInView(View):
+    def get(self, request):
+        form = AuthenticationForm()
+        context = {'signin_form': form}
+        return render(request, 'signin.html', context)
+    
+    def post(self, request):
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                form.add_error(None, "Invalid username or password.")
+        context = {'signin_form': form}
+        return render(request, 'signin.html', context)
+    
 # To Signout the  user
-def signout(request):
-    logout(request)
-    return render(request, 'signout.html')
+class SignOutView(View):
+    def get(self, request):
+        logout(request)
+        return render(request, 'signout.html')
 
 # To display the weather report
-def weather(request):
-    api_key = "333a8faa57184ef5534e17ed15f5d342"
-    city = "Madurai"
-    api_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
-    response = requests.get(api_url)
-    weather_data = response.json()
-    return render(request, "weather.html", {"weather_data": weather_data},)
 
 
-
+class WeatherView(View):
+    def get(self, request):
+        api_key = "333a8faa57184ef5534e17ed15f5d342"
+        city = "Madurai"
+        api_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+        response = requests.get(api_url)
+        weather_data = response.json()
+        return render(request, "weather.html", {"weather_data": weather_data})
